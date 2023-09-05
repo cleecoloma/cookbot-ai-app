deda9d484d1d69a367e721c0f4511da59918dc26
Spacecowboi <cunningham2192@gmail.com>
2023-09-05 10:56:38 -0700

    added more file scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { withAuth0 } from '@auth0/auth0-react';
+import Header from './Header';
 
 
 function App() {
@@ -10,13 +10,8 @@ function App() {
 
   return (
     <>
+     <Header />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -28,10 +23,9 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
       </p>
     </>
   )
 }
 
-export default App
+export default withAuth0(App);
