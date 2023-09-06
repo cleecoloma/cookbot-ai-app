475321075cf2eb1b8b3327ff726e1f7c148e41c1
Spacecowboi <cunningham2192@gmail.com>
2023-09-06 12:31:00 -0700

    added I thnk all auth0 necessary code

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+
+let AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN
+let AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Auth0Provider
+    domain={AUTH_DOMAIN}
+    clientId={AUTH_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: 'http://localhost:5173',
+    }}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
-)
+);
