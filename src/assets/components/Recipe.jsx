0b2e9dc3c032ee700187d023c9beaff05352cc30
David Danilchik <davdd@mac.com>
2023-09-06 11:50:23 -0700

    Added carousel for current recipes to be displayed."

--- src/assets/components/Recipe.jsx
+++ src/assets/components/Recipe.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import './Recipe.css'
+import Carousel from 'react-bootstrap/Carousel';
 import axios from "axios";
 import AddModal from "./AddModal";
 import { Button } from 'react-bootstrap';
@@ -8,31 +10,31 @@ const PORT = import.meta.env.VITE_SERVER_URL;
 
 class Recipe extends React.Component {
 
-    state = {
-        recipes: [],
-        showModal: false,
-    }
+  state = {
+      recipes: [],
+      showModal: false,
+  }
 
-    componentDidMount() {
-        this.fetchRecipes();
-    }
+  componentDidMount() {
+      this.fetchRecipes();
+  }
 
-    handleShowModal = () => {
-        this.setState({ showModal: true });
-    }
+  handleShowModal = () => {
+      this.setState({ showModal: true });
+  }
 
   handleCloseModal = () => {
     this.setState({ showModal: false });
   };
 
     //GET//
-    fetchRecipes = async () => {
-        axios.get(`${PORT}/recipes`)
-        .then(response => {
-            this.setState({recipes: response.data})
-            console.log(response.data)
-        });
-    }
+  fetchRecipes = async () => {
+      axios.get(`${PORT}/recipes`)
+      .then(response => {
+          this.setState({recipes: response.data})
+          console.log(response.data)
+      });
+  }
 
   //POST//
   addRecipe = (input) => {
@@ -78,15 +80,46 @@ class Recipe extends React.Component {
     render() {
         return (
             <div>
+  <Carousel>
+    {this.state.recipes.map((recipe, idx) => (
+        <Carousel.Item key={idx} className="carousel-item-custom">
+            <div className="d-flex justify-content-center align-items-center recipe-content">
+                <img
+                    className="img-fluid recipe-placeholder mx-3"
+                    src="https://placehold.co/600x400"
+                    alt="Recipe Image Placeholder"
+                />
+                <div>
                 <Button variant="outline-success" onClick={this.handleShowModal}>Add New Recipe</Button>
                 <AddModal 
                     show={this.state.showModal} 
                     onHide={this.handleCloseModal}
                     addRecipe={this.addRecipe}
                 />
+                    <h3 >{recipe.dishName}</h3>
+                    <ul>
+                        <li>
+                            <strong>{recipe.dishName}</strong>
+                        </li>
+                        <li >
+                            <strong>Ingredients:</strong>
+                            <ul>
+                                {recipe.ingredients && recipe.ingredients.map((ingredient, ingrIdx) => (
+                                    <li key={ingrIdx}>{ingredient}</li>
+                                ))}
+                            </ul>
+                        </li>
+                        {/* Add more items here as needed */}
+                    </ul>
+                </div>
             </div>
-        );
-    }
+        </Carousel.Item>
+    ))}
+  </Carousel>
+
+      </div>
+    );
+  }
 }
 
 export default Recipe;
