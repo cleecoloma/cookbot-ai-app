7abde1696bfa2e5f77431c9ce6adb886a75e769e
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-06 17:41:19 -0700

    added styling to header

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -5,21 +5,23 @@ import AuthButtons from '../../Auth_Folder/AuthButtons';
 class Header extends React.Component {
   render() {
     return (
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg" style={{ display:"flex", justifyContent:"space-between", padding:"0 5%" }}>
         <Navbar.Brand
-          style={{ margin:" 0 10%" }}
+          // style={{ margin:" 0 10%" }}
           href="/"
         >
           Welcome to CookBot!
         </Navbar.Brand>
+        <div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav >
             <Nav.Link href="/About.jsx">About Us</Nav.Link>
             <Nav.Link href="/Profile.jsx">Profile</Nav.Link>
           </Nav>
           <AuthButtons />
         </Navbar.Collapse>
+        </div>
       </Navbar>
     );
   }
