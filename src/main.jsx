bcdba4c6ea3bba4a42cee82088fc1767d29f04ae
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-07 17:57:55 -0700

    updated redirect_uri

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,13 @@ let AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
-    domain={AUTH_DOMAIN}
-    clientId={AUTH_CLIENT_ID}
-    authorizationParams={{
-      redirect_uri: 'http://localhost:5173',
-    }}
+      domain={AUTH_DOMAIN}
+      clientId={AUTH_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
     >
-    <App />
+      <App />
     </Auth0Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
