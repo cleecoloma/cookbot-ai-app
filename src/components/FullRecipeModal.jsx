9de2d1db8c4397b0492647bc39f6bbda7334b541
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-24 21:37:50 -0700

    updated location of default image

--- src/components/FullRecipeModal.jsx
+++ src/components/FullRecipeModal.jsx
@@ -38,7 +38,7 @@ class FullRecipeModal extends React.Component {
                   this.props.currentRecipe.timestamp
                 )
                   ? this.props.currentRecipe.imageUrl
-                  : '/images/cookbot-ai-default-img.png'
+                  : '/public/images/cookbot-ai-default-img.png'
               }
               alt={this.props.currentRecipe.name}
               style={{ width: '400px', height: '400px', objectFit: 'cover' }}
