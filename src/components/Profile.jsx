e78d2ecb4e06899c92882fa53264651272d34681
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-16 23:07:47 -0800

    updated Profile component to be function based

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,24 +3,22 @@ import Card from 'react-bootstrap/Card';
 import '../styles/Profile.css';
 import { withAuth0 } from '@auth0/auth0-react';
 
-class Profile extends React.Component {
-  render() {
-    const { user, isAuthenticated } = this.props.auth0;
-    return (
-      isAuthenticated && (
-        <div className="centered-container">
-          <h2>Profile</h2>
-          <Card id="profile-card" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={user.picture} />
-            <Card.Body>
-              <Card.Title>{user.nickname}</Card.Title>
-              <Card.Text>{user.email}</Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-      )
-    );
-  }
+function Profile(props) {
+  const { user, isAuthenticated } = props.auth0;
+  return (
+    isAuthenticated && (
+      <div className='centered-container'>
+        <h2>Profile</h2>
+        <Card id='profile-card' style={{ width: '18rem' }}>
+          <Card.Img variant='top' src={user.picture} />
+          <Card.Body>
+            <Card.Title>{user.nickname}</Card.Title>
+            <Card.Text>{user.email}</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    )
+  );
 }
 
 export default withAuth0(Profile);
