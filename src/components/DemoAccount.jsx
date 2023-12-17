e42858c6d4ef97b5c6310f7704f3942d9c926195
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-16 22:52:54 -0800

    updated DemoAccount component to be function based

--- src/components/DemoAccount.jsx
+++ src/components/DemoAccount.jsx
@@ -2,24 +2,19 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import '../styles/Profile.css';
 
-class DemoAccount extends React.Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-      <div className="centered-container">
-        <h2>Profile</h2>
-        <Card id="profile-card" style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={this.props.user.picture} />
-          <Card.Body>
-            <Card.Title>{this.props.user.nickname}</Card.Title>
-            <Card.Text>{this.props.user.email}</Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
-    );
-  }
+function DemoAccount(props) {
+  return (
+    <div className='centered-container'>
+      <h2>Profile</h2>
+      <Card id='profile-card' style={{ width: '18rem' }}>
+        <Card.Img variant='top' src={props.user.picture} />
+        <Card.Body>
+          <Card.Title>{props.user.nickname}</Card.Title>
+          <Card.Text>{props.user.email}</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default DemoAccount;
