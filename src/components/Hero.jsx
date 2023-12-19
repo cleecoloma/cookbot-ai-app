a4e21b9f65af453e32fc485ddb2907a6d951a4fd
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-18 16:24:01 -0800

    added slogon to hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,11 @@ import '../styles/Hero.css';
 function Hero() {
   return (
     <div id='hero-container'>
-      <div id='hero-left'>weedsfasfsfseee</div>
+      <div id='hero-left'>
+        <h2>
+          Infinite recipes, one click away
+        </h2>
+      </div>
       <div id='hero-right'>
         <img id='hero-image' src='../images/pizza.png'></img>
       </div>
