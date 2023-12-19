af653ffbb0e81089ab289daef5228dd5c921fbca
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-18 19:18:46 -0800

    added buttons on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,17 @@
 
 import React from 'react';
 import '../styles/Hero.css';
+import { Button } from 'react-bootstrap';
 
 function Hero() {
   return (
     <div id='hero-container'>
       <div id='hero-left'>
-        <h2>
-          Infinite recipes, one click away
-        </h2>
+        <h2>Infinite recipes, one click away</h2>
+        <div>
+          <Button variant='success'>See some recipes</Button>
+          <Button variant='success' id='try-it-button'>Try it</Button>
+        </div>
       </div>
       <div id='hero-right'>
         <img id='hero-image' src='../images/pizza.png'></img>
