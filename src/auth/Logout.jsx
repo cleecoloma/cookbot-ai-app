130a029f994ee500000161f3b6301fce3810d0ce
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-27 11:40:18 -0800

    updated hamburger menu styling

--- src/auth/Logout.jsx
+++ src/auth/Logout.jsx
@@ -2,23 +2,20 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from 'react-bootstrap';
 
+// Define handleLogout outside the LogoutButton component
+export function handleLogout(logout) {
+  logout({ returnTo: window.location.origin });
+}
+
 function LogoutButton() {
-
-  const {
-    isAuthenticated,
-    logout
-  } = useAuth0();
-
-  function handleLogout() {
-    logout({ returnTo: window.location.origin });
-  }
+  const { isAuthenticated, logout } = useAuth0();
 
   return (
     isAuthenticated && (
       <Button
         variant='secondary'
         id='logout-style-button'
-        onClick={handleLogout}
+        onClick={() => handleLogout(logout)} // Pass the logout function to handleLogout
       >
         Log out
       </Button>
--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
+import { LoginContext } from '../context/Login';
+import { useAuth0 } from '@auth0/auth0-react';
+import { handleLogout } from '../auth/Logout';
 import '../styles/HamburgerMenu.css';
 
 function HamburgerMenu() {
@@ -12,31 +16,87 @@ function HamburgerMenu() {
     setShowMenu(!showMenu);
   };
 
+  const { isAuthenticated, logout } = useAuth0();
+
+  const {
+    loggedUser,
+    isDemoAccount,
+    toggleLoginModal,
+    handleDemoLogout,
+    handleProfilePage,
+    handleAuthAccount,
+  } = useContext(LoginContext);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      handleAuthAccount();
+    }
+  }, [isAuthenticated]);
+
   return (
     <div id='hamburger-menu' onClick={handleMenuToggle}>
       {showMenu ? <CloseIcon id='close-button' /> : <MenuIcon />}
       {showMenu && (
         <Navbar>
-          <Nav
-            id='mobile-navigation-links'
-            className='me-auto'
-            variant='underline'
-          >
-            <Nav.Link href='#home' className='header-links'>
-              Home
-            </Nav.Link>
-            <Nav.Link href='#background' className='header-links'>
-              Background
-            </Nav.Link>
-            <Nav.Link href='#skills' className='header-links'>
-              Skills
-            </Nav.Link>
-            <Nav.Link href='#projects' className='header-links'>
-              Projects
-            </Nav.Link>
-            <Nav.Link href='#contact' className='header-links'>
-              Contact
-            </Nav.Link>
+          <Nav id='mobile-navigation-links' variant='pills'>
+            {isAuthenticated && (
+              <>
+                <Nav.Link as={NavLink} to='/profile' className='hamburger-link'>
+                  Welcome, {loggedUser ? loggedUser.nickname : null}
+                </Nav.Link>
+                <hr />
+              </>
+            )}
+            {isDemoAccount && (
+              <>
+                <Nav.Link
+                  as={NavLink}
+                  to='/profile'
+                  className='hamburger-link'
+                  onClick={handleProfilePage}
+                >
+                  Welcome, {loggedUser.nickname}
+                </Nav.Link>
+                <hr />
+              </>
+            )}
+            <>
+              <Nav.Link className='hamburger-link' as={NavLink} to='/'>
+                Home
+              </Nav.Link>
+              <hr />
+            </>
+            {isDemoAccount || isAuthenticated ? (
+              <>
+                <Nav.Link
+                  className='hamburger-link'
+                  as={NavLink}
+                  to='/my-recipes'
+                >
+                  My Recipes
+                </Nav.Link>
+                <hr />
+              </>
+            ) : null}
+
+            {!isDemoAccount && !isAuthenticated && (
+              <Nav.Link className='create-link' onClick={toggleLoginModal}>
+                Create a recipe
+              </Nav.Link>
+            )}
+            {isDemoAccount && (
+              <Nav.Link className='logout-link' onClick={handleDemoLogout}>
+                Logout
+              </Nav.Link>
+            )}
+            {isAuthenticated && (
+              <Nav.Link
+                className='logout-link'
+                onClick={() => handleLogout(logout)}
+              >
+                Logout
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar>
       )}
