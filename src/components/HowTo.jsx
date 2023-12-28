17373565e076c263260ea35647b2678b1341d774
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-27 16:01:46 -0800

    updated added how-to instruction gifs

--- src/components/HowTo.jsx
+++ src/components/HowTo.jsx
@@ -6,10 +6,22 @@ import '../styles/HowTo.css';
 function HowTo({ id }) {
   return (
     <div className='how-to' id={id}>
-      <h3>
-        How it works?
-      </h3>
-      <img src='https://placehold.co/500x600' />
+      <h3>How it works?</h3>
+      <img
+        className='how-to-image how-to-desktop'
+        src='../../images/how-to-shrimp.gif'
+        alt='How it works for desktop'
+      />
+      <img
+        className='how-to-image how-to-tablet'
+        src='../../images/how-to-shrimp-tablet.gif'
+        alt='How it works for tablet'
+      />
+      <img
+        className='how-to-image how-to-mobile'
+        src='../../images/how-to-shrimp-mobile.gif'
+        alt='How it works for mobile'
+      />
     </div>
   );
 }
